6a170848abe2fd590499288929d9d673751c0d27
Sean Moore <sean.m.moore@gmail.com>
2019-07-04 21:45:02 -0700

    Move utility back into boid.c as static functions.

--- boid.c
+++ boid.c
@@ -1,8 +1,39 @@
 #include "boid.h"
-#include "boid_utility.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
+/**
+ * Return a copy of the vector with the magnitude restricted to at most max.
+ */
+static Vector2 Vector2Clip(Vector2 v, float max) {
+    float length = Vector2Length(v);
+    if (length > max) {
+        float r = length / max;
+        v = Vector2Divide(v, r);
+    }
+    return v;
+}
+
+/**
+ * Return a random float normalized to the interval (0, 1).
+ */
+static float rand_normalized() {
+    return (float) rand() / (float) RAND_MAX;
+}
+
+/**
+ * Wrap around the given value to fit between min and max.
+ */
+static float wrap(float value, float min, float max) {
+    if (value < min) {
+        value += (max - min);
+    }
+    else if (value > max) {
+        value -= (max - min);
+    }
+    return value;
+}
 
 struct boid_state *boid_state_new(struct boid_parameters *p) {
     struct boid_state *state = malloc(sizeof(struct boid_state));
