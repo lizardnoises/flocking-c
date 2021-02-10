29cfcfef8bb3da0bc3d63b7dc63ce74f5b43df4a
Sean Moore <sean.m.moore@gmail.com>
2021-02-09 19:29:49 -0800

    Clean up unused files that should have been created on a new branch.

--- src/entities.c
+++
@@ -1,7 +0,0 @@
-#include "entities.h"
-#include "components.h"
-#include "stdarg.h"
-
-void init_entity_manager(struct entity_manager *manager) {
-    memset(manager->flags, 0, sizeof(manager->flags));
-}
